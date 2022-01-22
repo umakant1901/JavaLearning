c4755c48860ce9f923f79d14cb95a29c585bf49f
Rudra <umakant.sb.pandey@gmail.com>
2022-01-22 20:36:29 +0530

    added basic structure

---
+++ src/com/umakant/JavaLearning/Main.java
@@ -0,0 +1,8 @@
+package com.umakant.JavaLearning;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hey, This is my first Java programs.");
+    }
+}
